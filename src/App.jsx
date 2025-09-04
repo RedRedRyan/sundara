5c0ba9f3f2036d2fa460bb926f4118a62e6ad1a6
RedRedRyan <ryankipkorir2002@gmail.com>
2025-09-04 22:56:02 +0300

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/NavBar'
 import Contact from './components/Contact'
 import Hero from './components/Hero'
 import Tech from "./components/Tech.jsx";
-import './App.css'
 import About from "./components/About.jsx";
 
 
